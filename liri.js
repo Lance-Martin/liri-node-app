b40771ad5e71f0aa94db4e76b34a25092084529c
Lance Martin <ldmartin7777@gmail.com>
2016-06-26 20:29:14 -0500

    added packages

---
+++ liri.js
@@ -0,0 +1,10 @@
+var key = require('keys.js');
+var spotify = require('spotify');
+var request = require('request');
+var twitter = require('twitter');
+switch (process.argv[2]) {
+    case 'my-tweets':
+    case 'spotify-this-song':
+    case 'movie-this':
+    case 'do-what-it-says':
+}
